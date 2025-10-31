ef61b23ca8e5537ec671f07662308c0ce363acb9
MINWOO KIM <sunnybikers@naver.com>
2025-10-31 20:35:09 +0900

    test: 로또와 로또들 생성 테스트
    - 로또 하나를 생성할 때 로또 번호들의 유효성 검사를 해야한다
    - 로또가 여러 개인 로또들은 구매금액만큼 잘 생성된다

---
+++ src/test/java/lotto/domain/LottosTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LottosTest {
+
+    @Test
+    void 로또를_구매금액만큼_만든다() {
+        // given
+        int count = 10000;
+        int money = 1000;
+
+        // when
+        Lottos lottos = Lottos.of(count * money, new LottoFactory());
+
+        // then
+        Assertions.assertThat(lottos.getLottos().size()).isEqualTo(count);
+    }
+
+  
+}
