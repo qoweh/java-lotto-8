954de0baa24fe334734df0eb6b2e529576840614
MINWOO KIM <rlaalsdn110@gmail.com>
2025-11-03 17:43:51 +0900

    refactor: 가독성 향상
    - 상수의 네이밍을 코드 파악에 용이하게 변경
    - 암묵적으로 소수점 첫째자리까지 나오도록 했지만 명시적으로 코드로 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,9 +7,9 @@ public class OutputView {
     private static final String PURCHASE_MESSAGE_PREFIX = "\n";
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
     private static final String RESULT_NOTIFY_MESSAGE = "\n당첨 통계\n---";
-    private static final String RESULT_COUNT_MESSAGE_PREFIX = "개 일치 (";
-    private static final String RESULT_COUNT_MESSAGE_MIDDLE = "원) - ";
-    private static final String RESULT_COUNT_MESSAGE_SUFFIX = "개";
+    private static final String RESULT_TARGET_COUNT_MESSAGE = "개 일치 (";
+    private static final String RESULT_PRIZE_MESSAGE = "원) - ";
+    private static final String RESULT_RESULT_COUNT_MESSAGE = "개";
     private static final String RESULT_RATE_MESSAGE_PREFIX = "총 수익률은 ";
     private static final String RESULT_RATE_MESSAGE_SUFFIX = "%입니다.";
     private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance();
@@ -29,20 +29,27 @@ public class OutputView {
     }
 
     public static void count(int targetCount, int prize, int resultCount) {
-        String targetCountMessage = RESULT_COUNT_MESSAGE_PREFIX;
-        if (targetCount == previousTargetCount) {
-            targetCountMessage = "개 일치, 보너스 볼 일치 (";
+        String resultTargetCountMessage = RESULT_TARGET_COUNT_MESSAGE;
+        if (isSecondTargetCount(targetCount)) {
+            resultTargetCountMessage = "개 일치, 보너스 볼 일치 (";
         }
 
-        System.out.println(
-                targetCount + targetCountMessage
-                + NUMBER_FORMAT.format(prize) + RESULT_COUNT_MESSAGE_MIDDLE
-                + resultCount + RESULT_COUNT_MESSAGE_SUFFIX);
+        System.out.println(targetCount + resultTargetCountMessage
+                + NUMBER_FORMAT.format(prize) + RESULT_PRIZE_MESSAGE
+                + resultCount + RESULT_RESULT_COUNT_MESSAGE
+        );
 
         previousTargetCount = targetCount;
     }
 
+    private static boolean isSecondTargetCount(int targetCount) {
+        return targetCount == previousTargetCount;
+    }
+
     public static void rate(double rate) {
-        System.out.println(RESULT_RATE_MESSAGE_PREFIX + rate + RESULT_RATE_MESSAGE_SUFFIX);
+        System.out.println(RESULT_RATE_MESSAGE_PREFIX
+                + String.format("%.1f", rate)
+                + RESULT_RATE_MESSAGE_SUFFIX
+        );
     }
 }
